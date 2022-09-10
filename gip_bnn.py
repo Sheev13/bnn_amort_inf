60c5907ed532d18be03835184bec187d4ba78ebd
Sheev13 <rochussent@gmail.com>
2022-09-10 11:24:21 +0100

    generalised params over w & b

--- gip_bnn.py
+++ gip_bnn.py
@@ -5,17 +5,17 @@ from torch import nn
 class GILayer(nn.Module):
     """Represents a single layer of a Bayesian neural network with global inducing points"""
 
-    def __init__(self, input_dim, output_dim, num_induce):
+    def __init__(self, input_dim, output_dim, num_induce, nonlinearity=nn.ReLU()):
         super(GILayer, self).__init__()
         self.input_dim = input_dim
         self.output_dim = output_dim
         self.num_induce = num_induce
+        self.nonlinearity = nonlinearity
 
         # priors
-        self.w_mu_p = torch.zeros(input_dim, output_dim)
-        self.w_logvar_p = torch.zeros(input_dim, output_dim)
-        self.b_mu_p = torch.zeros(input_dim, output_dim)
-        self.b_logvar_p = torch.zeros(input_dim, output_dim)
+        self.mu_p = torch.zeros(input_dim, output_dim)
+        self.logvar_p = torch.zeros(input_dim, output_dim)
+        self.prior = torch.distributions.Normal(self.mu_p, (0.5 * self.logvar_p).exp())
 
         # do we need posterior init here?
 
@@ -24,7 +24,11 @@ class GILayer(nn.Module):
         self.pseud_logprec = nn.Parameter(torch.zeros(num_induce))
 
     def q_prec(self, U_in):
-        pass
+        psi_U = self.nonlinearity(U_in)
+        pseud_prec = torch.exp(self.pseud_logprec)
+        
+        psi_U_T_lambda_psi_U = torch.einsum("mi, m mj -> ij", psi_U, pseud_prec, psi_U)
+        q_prec = torch.diag(torch.exp(-self.logvar_p)[:, 0]) + psi_U_T_lambda_psi_U
 
     def q_mu(self, U_in, q_prec):
         pass
