efc09c1c7f4355e1801526f1d378634e913bebe8
Sheev13 <rochussent@gmail.com>
2022-09-12 10:15:01 +0100

    initial buggy implementation of GIP BNN

--- gip_bnn.py
+++ gip_bnn.py
@@ -45,7 +45,7 @@ class GILayer(nn.Module):
         psi_U = self.nonlinearity(U_in)
         pseud_prec = torch.exp(self.pseud_logprec)
         
-        # what should the shape be?
+        # what should the shape of this be?
         return q_cov @ psi_U.T @ pseud_prec @ self.pseud_mu  # perhaps use einsum instead?
         
     def forward(self, F_in, U_in):
@@ -64,28 +64,28 @@ class GILayer(nn.Module):
         w = q.sample()
         
         kl_contribution = torch.distributions.kl.kl_divergence(q, self.prior).sum()
-        log_p = self.prior.log_prob(w).sum()
-        log_q = q.log_prob(w).sum()
         
         F_out = self.nonlinearity(F_in) @ w  # check that this is the right way around
         U_out = self.nonlinearity(U_in) @ w  # check that this is the right way around
         
-        return F_out, U_out, kl_contribution, log_p, log_q
+        return F_out, U_out, kl_contribution
 
 
 
 
 
 class GINetwork(nn.Module):
-    """Represents the full network"""
+    """Represents the full Global Inducing Point BNN"""
     
-    def __init__(self, input_dim, hidden_dims, output_dim, num_induce, nonlinearity=nn.ELU()):
+    def __init__(self, input_dim, hidden_dims, output_dim, inducing_points, nonlinearity=nn.ELU()):
         super(GINetwork, self).__init__()
         self.input_dim = input_dim
         self.hidden_dims = hidden_dims
         self.output_dim = output_dim
-        self.num_induce = num_induce
+        self.inducing_points = nn.Parameter(inducing_points)
+        self.num_induce = inducing_points.shape[0]
         self.nonlinearity = nonlinearity
+        self.log_noise = nn.Parameter(torch.tensor(-1))
         
         self.network = nn.ModuleList()
         for i in range(len(hidden_dims) + 1):
@@ -104,11 +104,33 @@ class GINetwork(nn.Module):
                                             self.hidden_dims[i],
                                             num_induce=self.num_induce,
                                             nonlinearity=self.nonlinearity))
+         
+    @property            
+    def noise(self):
+        return torch.exp(self.log_noise)            
                 
-    def forward(self):
-        pass
+    def forward(self, F):
+        kl_total = torch.tensor(0)
+        for i, layer in enumerate(self.network):
+            if i == 0:
+                F, U, kl = layer(F, self.inducing_points)
+            else:
+                F, U, kl = layer(F, U)
+            kl_total += kl
+            
+        means = F
+        
+        return means, self.noise, kl_total
+            
+    def ll(self, means, y):
+        scales = self.noise * torch.ones_like(means)
+        l = torch.distributions.normal.Normal(means, scales)
+        return l.log_prob(y).sum()
     
-    def elbo(self):
-        pass
+    def elbo(self, x, y):
+        means, noise, kl = self(x)
+        ll = self.ll(means, y)
+        elbo = ll - kl
+        return elbo, ll, kl, noise
     
     
