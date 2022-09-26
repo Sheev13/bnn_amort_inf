e589099afe0b2e4fedb84c234f8c140df4616ea7
Sheev13 <rochussent@gmail.com>
2022-09-26 18:52:26 +0100

    initial implementation done

--- amort_bnn.py
+++ amort_bnn.py
@@ -17,36 +17,26 @@ class InferenceNetwork(nn.Module):
         self.activation = activation
         
         self.network = nn.ModuleList()
-        for i in range(len(hidden_dims) + 1):
-            if i == 0:
-                self.network.append(
-                    nn.Linear(
-                        self.input_dim,
-                        self.hidden_dims[i],
-                    )
+        
+        self.network.append(nn.Linear(self.input_dim, self.hidden_dims[0]))
+        self.network.append(self.activation)
+        
+        for i in range(1, len(hidden_dims)):
+            self.network.append(
+                nn.Linear(
+                    self.hidden_dims[i - 1],
+                    self.hidden_dims[i],
                 )
-                self.network.append(self.activation)
-            elif i == len(hidden_dims):
-                self.network.append(
-                    nn.Linear(
-                        self.hidden_dims[i - 1],
-                        self.output_dim,
-                    )
-                )
-                self.network.append(nn.Identity())
-            else:
-                self.network.append(
-                    nn.Linear(
-                        self.hidden_dims[i - 1],
-                        self.hidden_dims[i],
-                    )
-                )
-                self.network.append(self.activation)
+            )
+            self.network.append(self.activation)
+            
+        self.network.append(nn.Linear(self.hidden_dims[-1], self.output_dim))
+        self.network.append(nn.Identity())
                 
-    def forward(self, U):
+    def forward(self, z):
         for layer in self.network:
-            U = layer(U)
-        return U
+            z = layer(z)
+        return z
 
 
 
@@ -73,11 +63,24 @@ class AmortLayer(nn.Module):
             self.mu_p, self.var_p.diag_embed()
         )
         
-        # auxiliary network
-        self.aux_network = InferenceNetwork(self.output_dim * 2)
-
-    def get_q(self, U: torch.Tensor) -> torch.distributions.MultivariateNormal:
-        # U is shape (num_samples, num_induces, input_dim).
+        # amortising/auxiliary inference network
+        self.inference_network = InferenceNetwork(self.output_dim * 2)
+        
+    def infer_pseudos(self, x, y):
+        # z is shape (batch_size, 2)
+        z = torch.cat((x, y), dim=1)
+        # take first output_dim NN outputs as means and last output_dim NN outputs as log stds
+        # pseud_mu & pseud_logstd are shape (batch_size, output_dim)
+        pseud_mu, pseud_logstd = torch.split(
+            self.inference_network(z),
+            self.output_dim,
+            dim=2,
+        )
+        pseud_prec = 1 / ((2 * pseud_logstd).exp())
+        return pseud_mu, pseud_prec
+    
+    def get_q(self, U: torch.Tensor, x, y) -> torch.distributions.MultivariateNormal:
+        # U is shape (num_samples, N, input_dim).
         assert len(U.shape) == 3
         # assert U.shape[1] == self.num_induce
         assert U.shape[2] == self.input_dim
@@ -85,13 +88,11 @@ class AmortLayer(nn.Module):
         # U_ is shape (num_samples, 1, num_induce, input_dim).
         U_ = U.unsqueeze(1)
 
-        # amortisation here: will need to sort this out
-        pseud_logprec, pseud_mu = self.aux_network(U)
-        
-        # what shape are pseud_logprec and pseud_mu?
+        # amortisation
+        pseud_mu, pseud_prec = self.infer_pseudos(x, y)
         
         # pseud_prec_ is shape (1, output_dim, 1, num_induce).
-        pseud_prec_ = pseud_logprec.exp().unsqueeze(0).unsqueeze(-2)
+        pseud_prec_ = pseud_prec.unsqueeze(0).unsqueeze(-2)
 
         # pseud_mu_ is shape (1, output_dim, num_induce, 1).
         pseud_mu_ = pseud_mu.unsqueeze(0).unsqueeze(-1)
@@ -114,11 +115,13 @@ class AmortLayer(nn.Module):
         q_mu = (q_cov @ UTLv).squeeze(-1)
         return torch.distributions.MultivariateNormal(q_mu, q_cov)
 
-    def forward(self, U):
+    def forward(self, F, U, x, y):
         assert len(U.shape) == 3
+        assert len(F.shape) == 3
         assert U.shape[2] == self.input_dim
+        assert F.shape[2] == self.input_dim
 
-        q = self.get_q(U)
+        q = self.get_q(U, x, y)
 
         # w should be shape (num_samples, output_dim, input_dim).
         w = q.rsample()
@@ -128,10 +131,11 @@ class AmortLayer(nn.Module):
             -1
         )
 
-        # U is shape (num_samples, batch_size, output_dim).
+        # F is shape (num_samples, batch_size, output_dim).
+        F = self.activation((F @ w.transpose(-1, -2)))
         U = self.activation((U @ w.transpose(-1, -2)))
 
-        return U, kl_contribution
+        return F, U, kl_contribution
 
 
 class AmortNetwork(nn.Module):
@@ -142,7 +146,8 @@ class AmortNetwork(nn.Module):
         input_dim,
         hidden_dims,
         output_dim,
-        inducing_points,
+        x,
+        y,
         nonlinearity=nn.ELU(),
         prior_var=1.0,
         init_noise=1e-1,
@@ -152,14 +157,15 @@ class AmortNetwork(nn.Module):
         self.input_dim = input_dim
         self.hidden_dims = hidden_dims
         self.output_dim = output_dim
-        self.inducing_points = nn.Parameter(inducing_points)
-        self.num_induce = inducing_points.shape[0]
+        self.x = x
+        self.y = y
         self.nonlinearity = nonlinearity
         self.prior_var = prior_var
         self.log_noise = nn.Parameter(
             torch.tensor(init_noise).log(), requires_grad=trainable_noise
         )
 
+        #TODO: clean this for loop up as Boris advised
         self.network = nn.ModuleList()
         for i in range(len(hidden_dims) + 1):
             if i == 0:
@@ -203,7 +209,7 @@ class AmortNetwork(nn.Module):
 
         # (num_samples, batch_size, input_dim).
         F = F.unsqueeze(0).repeat(num_samples, 1, 1)
-        U = self.inducing_points.unsqueeze(0).repeat(num_samples, 1, 1)
+        U = self.x.unsqueeze(0).repeat(num_samples, 1, 1)
 
         kl_total = None
         for layer in self.network:
@@ -212,7 +218,7 @@ class AmortNetwork(nn.Module):
             U_ones = torch.ones(U.shape[:-1]).unsqueeze(-1)
             U = torch.cat((U, U_ones), dim=-1)
 
-            F, U, kl = layer(F, U)
+            F, U, kl = layer(F, U, self.x, self.y)
 
             if kl_total is None:
                 kl_total = kl
@@ -249,8 +255,11 @@ class AmortNetwork(nn.Module):
         elbo = ll - kl
         return -elbo, ll, kl, self.noise
     
-    def get_pseudos(self):
-        locs = self.inducing_points.detach().squeeze()
-        final_layer = self.network[-1]
-        outputs = final_layer.pseud_mu.detach().squeeze()
-        return locs, outputs
+    
+    # will need to fix this up later
+    
+    # def get_pseud_outs(self):
+    #     locs = self.x.squeeze()
+    #     final_layer = self.network[-1]
+    #     outputs = final_layer.pseud_mu.detach().squeeze()
+    #     return locs, outputs
