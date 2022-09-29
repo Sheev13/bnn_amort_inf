9ae56a861de6d1b5e101f5fb48bf6c043e642d87
M.C. Ashman <mca39@ulam.eng.cam.ac.uk>
2022-09-29 15:56:42 +0100

    Updated some stuff

--- amort_bnn.py
+++ amort_bnn.py
@@ -1,9 +1,10 @@
 import torch
 from torch import nn
 
+
 class InferenceNetwork(nn.Module):
     """Represents a network that implements amortisation within a layer of the wider network."""
-    
+
     def __init__(
         self,
         output_dim,
@@ -15,12 +16,12 @@ class InferenceNetwork(nn.Module):
         self.hidden_dims = hidden_dims
         self.output_dim = output_dim
         self.activation = activation
-        
+
         self.network = nn.ModuleList()
-        
+
         self.network.append(nn.Linear(self.input_dim, self.hidden_dims[0]))
         self.network.append(self.activation)
-        
+
         for i in range(1, len(hidden_dims)):
             self.network.append(
                 nn.Linear(
@@ -29,15 +30,14 @@ class InferenceNetwork(nn.Module):
                 )
             )
             self.network.append(self.activation)
-            
+
         self.network.append(nn.Linear(self.hidden_dims[-1], self.output_dim))
         self.network.append(nn.Identity())
-                
+
     def forward(self, z):
         for layer in self.network:
             z = layer(z)
         return z
-
 
 
 class AmortLayer(nn.Module):
@@ -68,14 +68,15 @@ class AmortLayer(nn.Module):
         self.full_prior = torch.distributions.MultivariateNormal(
             self.mu_p, self.var_p.diag_embed()
         )
-        
+
         # amortising/auxiliary inference network
         if self.infer_last_pseudos:
-            self.inference_network = InferenceNetwork(self.output_dim * 2,
-                                                      hidden_dims=self.inf_net_dims,
-                                                      activation=self.inf_net_act,
-                                                      )
-        
+            self.inference_network = InferenceNetwork(
+                self.output_dim * 2,
+                hidden_dims=self.inf_net_dims,
+                activation=self.inf_net_act,
+            )
+
     def infer_pseudos(self, x, y):
         # z is shape (batch_size, 2)
         z = torch.cat((x, y), dim=1)
@@ -88,15 +89,15 @@ class AmortLayer(nn.Module):
         )
         pseud_prec = 1 / ((2 * pseud_logstd).exp())
         return pseud_mu.T, pseud_prec.T
-    
-    def get_q(self, U: torch.Tensor, x, y, noise=None) -> torch.distributions.MultivariateNormal:
+
+    def get_q(self, x, y, F, noise=None) -> torch.distributions.MultivariateNormal:
         # U is shape (num_samples, N, input_dim).
-        assert len(U.shape) == 3
+        assert len(F.shape) == 3
         # assert U.shape[1] == self.batch_size
-        assert U.shape[2] == self.input_dim
+        assert F.shape[2] == self.input_dim
 
         # U_ is shape (num_samples, 1, batch_size, input_dim).
-        U_ = U.unsqueeze(1)
+        F_ = F.unsqueeze(1)
 
         # amortisation
         if self.infer_last_pseudos:
@@ -104,41 +105,43 @@ class AmortLayer(nn.Module):
         else:
             pseud_mu = y.T
             pseud_prec = (1 / (noise**2)) * torch.ones_like(pseud_mu)
-        
+
         # pseud_prec_ is shape (1, output_dim, 1, batch_size).
         pseud_prec_ = pseud_prec.unsqueeze(0).unsqueeze(-2)
 
         # pseud_mu_ is shape (1, output_dim, batch_size, 1).
         pseud_mu_ = pseud_mu.unsqueeze(0).unsqueeze(-1)
 
-        # UTL is shape (num_samples, output_dim, input_dim, batch_size)
-        UTL = U_.transpose(-1, -2) * pseud_prec_
+        # FTL is shape (num_samples, output_dim, input_dim, batch_size)
+        FTL = F_.transpose(-1, -2) * pseud_prec_
 
-        # UTLU is shape (num_samples, output_dim, input_dim, input_dim)
-        UTLU = UTL @ U_
+        # FTLF is shape (num_samples, output_dim, input_dim, input_dim)
+        FTLF = FTL @ F_
 
-        # UTLv is shape (num_samples, output_dim, input_dim, 1)
-        UTLv = UTL @ pseud_mu_
+        # FTLv is shape (num_samples, output_dim, input_dim, 1)
+        FTLv = FTL @ pseud_mu_
 
         # prior_prec_ is shape (1, output_dim, input_dim, input_dim)
         prior_prec_ = (self.var_p ** (-1)).diag_embed().unsqueeze(0)
 
-        q_prec = prior_prec_ + UTLU
+        q_prec = prior_prec_ + FTLF
         q_prec_chol = torch.linalg.cholesky(q_prec)
         q_cov = torch.cholesky_inverse(q_prec_chol)
-        q_mu = (q_cov @ UTLv).squeeze(-1)
+        q_mu = (q_cov @ FTLv).squeeze(-1)
         return torch.distributions.MultivariateNormal(q_mu, q_cov)
 
-    def forward(self, F, U, x, y, noise=None):
-        assert len(U.shape) == 3
+    def forward(self, x, y, F, F_test=None, noise=None):
         assert len(F.shape) == 3
-        assert U.shape[2] == self.input_dim
         assert F.shape[2] == self.input_dim
+
+        if F_test is not None:
+            assert len(F_test.shape) == 3
+            assert F_test.shape[2] == self.input_dim
 
         if self.infer_last_pseudos:
             assert noise is None
-            
-        q = self.get_q(U, x, y, noise)
+
+        q = self.get_q(x, y, F, noise)
 
         # w should be shape (num_samples, output_dim, input_dim).
         w = q.rsample()
@@ -150,9 +153,11 @@ class AmortLayer(nn.Module):
 
         # F is shape (num_samples, batch_size, output_dim).
         F = self.activation((F @ w.transpose(-1, -2)))
-        U = self.activation((U @ w.transpose(-1, -2)))
 
-        return F, U, kl_contribution
+        if F_test is not None:
+            F_test = self.activation((F_test @ w.transpose(-1, -2)))
+
+        return F, F_test, kl_contribution
 
 
 class AmortNetwork(nn.Module):
@@ -163,13 +168,11 @@ class AmortNetwork(nn.Module):
         input_dim,
         hidden_dims,
         output_dim,
-        x,
-        y,
         nonlinearity=nn.ReLU(),
         prior_var=1.0,
         init_noise=1e-1,
         trainable_noise=True,
-        inf_net_dims=[100, 100],
+        inf_net_dims=[20, 20],
         inf_net_act=nn.ReLU(),
         infer_last_pseudos=False,
     ):
@@ -177,19 +180,18 @@ class AmortNetwork(nn.Module):
         self.input_dim = input_dim
         self.hidden_dims = hidden_dims
         self.output_dim = output_dim
-        self.x = x
-        self.y = y
         self.nonlinearity = nonlinearity
+
         self.prior_var = prior_var
         self.log_noise = nn.Parameter(
             torch.tensor(init_noise).log(), requires_grad=trainable_noise
         )
+
         self.inf_net_dims = inf_net_dims
         self.inf_net_act = inf_net_act
         self.infer_last_pseudos = infer_last_pseudos
 
         self.network = nn.ModuleList()
-        
         self.network.append(
             AmortLayer(
                 self.input_dim + 1,
@@ -227,27 +229,33 @@ class AmortNetwork(nn.Module):
     def noise(self):
         return torch.exp(self.log_noise)
 
-    def forward(self, F, num_samples=1):
-        assert len(F.shape) == 2
-        assert F.shape[1] == self.input_dim
+    def forward(self, x, y, x_test=None, num_samples=1):
+        assert len(x.shape) == 2
+        assert x.shape[1] == self.input_dim
 
         # (num_samples, batch_size, input_dim).
-        F = F.unsqueeze(0).repeat(num_samples, 1, 1)
-        U = self.x.unsqueeze(0).repeat(num_samples, 1, 1)
+        F = x.unsqueeze(0).repeat(num_samples, 1, 1)
+
+        if x_test is not None:
+            F_test = x_test.unsqueeze(0).repeat(num_samples, 1, 1)
+        else:
+            F_test = None
 
         kl_total = None
         for layer in self.network:
             F_ones = torch.ones(F.shape[:-1]).unsqueeze(-1)
             F = torch.cat((F, F_ones), dim=-1)
-            U_ones = torch.ones(U.shape[:-1]).unsqueeze(-1)
-            U = torch.cat((U, U_ones), dim=-1)
+
+            if F_test is not None:
+                F_test_ones = torch.ones(F_test.shape[:-1]).unsqueeze(-1)
+                F_test = torch.cat((F_test, F_test_ones), dim=-1)
 
             if layer.infer_last_pseudos:
                 noise = None
             else:
                 noise = self.noise
-                
-            F, U, kl = layer(F, U, self.x, self.y, noise)
+
+            F, F_test, kl = layer(x, y, F, F_test, noise)
 
             if kl_total is None:
                 kl_total = kl
@@ -260,7 +268,7 @@ class AmortNetwork(nn.Module):
         assert F.shape[0] == num_samples
         assert F.shape[2] == self.output_dim
 
-        return F, kl_total
+        return F, F_test, kl_total
 
     def ll(self, F, y):
         num_samples = F.shape[0]
@@ -273,7 +281,7 @@ class AmortNetwork(nn.Module):
         return log_prob.sum(1).sum(1)
 
     def elbo_loss(self, x, y, num_samples=1):
-        F, kl = self(x, num_samples=num_samples)
+        F, _, kl = self(x, y, x_test=None, num_samples=num_samples)
         ll = self.ll(F, y)
         assert len(ll.shape) == 1
         assert ll.shape[0] == num_samples
@@ -283,12 +291,11 @@ class AmortNetwork(nn.Module):
         kl = kl.mean()
         elbo = ll - kl
         return -elbo, ll, kl, self.noise
-    
-    def get_pseud_outs(self):
-        locs = self.x.squeeze()
-        if self.infer_last_pseudos:
-            final_layer = self.network[-1]
-            outputs = final_layer.infer_pseudos(self.x, self.y)[0].detach().squeeze()
-        else:
-            outputs = self.y.squeeze()
-        return locs, outputs 
+
+    # def get_pseud_outs(self):
+    #     if self.infer_last_pseudos:
+    #         final_layer = self.network[-1]
+    #         outputs = final_layer.infer_pseudos(self.x, self.y)[0].detach().squeeze()
+    #     else:
+    #         outputs = self.y.squeeze()
+    #     return locs, outputs
