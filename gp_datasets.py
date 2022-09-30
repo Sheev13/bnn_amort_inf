1d26ebb5b086f7b4da703c4265dd6b41bdd5dcfe
Sheev13 <rochussent@gmail.com>
2022-09-30 18:09:52 +0100

    new module for GP datasets

---
+++ gp_datasets.py
@@ -0,0 +1,8 @@
+import gpytorch
+
+class GPDataGenerator():
+    """Represents a box of tricks used to generate toy datasets used as meta-learning tasks"""
+    
+    def __init__(self):
+        pass
+
