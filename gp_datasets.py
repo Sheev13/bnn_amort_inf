223793bd19eddecdc52b84454b4956468c25857c
Sheev13 <rochussent@gmail.com>
2022-10-04 18:02:54 +0100

    started gp dataset class

--- gp_datasets.py
+++ gp_datasets.py
@@ -1,8 +1,19 @@
 import gpytorch
+import torch
 
 class GPDataGenerator():
     """Represents a box of tricks used to generate toy datasets used as meta-learning tasks"""
     
     def __init__(self):
+        self.gp = gpytorch.models.ExactGP(
+            torch.Tensor(0.0),
+            torch.Tensor(0.0),
+            gpytorch.likelihoods.GaussianLikelihood(),
+        )
+        
+    def generate_tasks(
+        self,
+        num_tasks,
+    ):
         pass
 
