ed01920290c28ec3b33c567edfa0a00805fa3561
Sheev13 <rochussent@gmail.com>
2022-10-25 00:03:15 +0100

    more work on amortised mfvi bnn

--- bnn_amort_inf/models/amortised_mfvi_bnn.py
+++ bnn_amort_inf/models/amortised_mfvi_bnn.py
@@ -36,7 +36,7 @@ class AmortisedMFVIBNNLayer(nn.Module):
         self._cache: Dict = {}
 
         self.inference_network = MLP(
-            [x_dim + y_dim] + in_hidden_dims + [2 * output_dim],
+            [x_dim + y_dim] + in_hidden_dims + [2 * input_dim * output_dim],
             nonlinearity=in_nonlinearity,
             activation=NormalActivation(),
         )
@@ -44,6 +44,24 @@ class AmortisedMFVIBNNLayer(nn.Module):
     @property
     def cache(self):
         return self._cache
+
+    def qw(self, x: torch.Tensor, y: torch.Tensor):
+        # w_dist is shape (batch_size, input_dim * output_dim)
+        w_dist = self.inference_network(torch.cat((x, y), dim=-1))
+        # product over datapoints
+        w_dist = w_dist.prod(dim=0)
+        # reshape to match prior
+        w_dist = w_dist.reshape((self.output_dim, self.input_dim))
+
+        q = self.pw @ w_dist
+        assert len(q.shape) == 2
+        assert q.shape[0] == self.output_dim
+        assert q.shape[1] == self.input_dim
+
+        return q
+
+    def forward(x: torch.Tensor, y: torch.Tensor, x_test: torch.Tensor):
+        pass
 
     class AmortisedMFVIBNN(nn.Module):
         """Represents the full amortised factorised-Gaussian BNN"""
